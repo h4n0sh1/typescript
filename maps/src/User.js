cb0347c8124540810a02b661bb45cbac50fa1a80
h4n0s1 <hanchi.oth@gmail.com>
2024-10-12 20:17:02 +0200

    Add user class and new  dependecies. Add new folder fundamentals.

---
+++ maps/src/User.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+var faker_1 = require("@faker-js/faker");
+var User = /** @class */ (function () {
+    function User() {
+        this.name = faker_1.faker.person.firstName();
+        this.location = {
+            lat: faker_1.faker.location.latitude(),
+            lng: faker_1.faker.location.longitude()
+        };
+    }
+    return User;
+}());
+exports.User = User;
--- maps/src/index.js
+++ maps/src/index.js
@@ -1,1 +1,5 @@
-console.log('hi there!');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User_1 = require("./User");
+var user = new User_1.User();
+console.log(user);
