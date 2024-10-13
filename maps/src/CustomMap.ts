b98c5784610d3777041030eaef5470de75cc4afc
h4n0s1 <hanchi.oth@gmail.com>
2024-10-13 12:53:20 +0200

    Add map markers

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,13 +1,52 @@
+import { User } from './User'
+import { Company } from './Company'
+
+interface Mappable{
+    location: {
+        lat: number;
+        lng: number;
+    };
+}
 export class CustomMap {
     private googleMap: google.maps.Map;
 
     constructor(id: string) {
+        //this.initMap(id);
         this.googleMap = new google.maps.Map(document.getElementById(id) as HTMLElement, {
             zoom: 1,
             center: {
                 lat: 0,
                 lng: 0
-            }
+            },
+            mapId: 'CustomMap'
         });
     }
+
+    // Other way to initialize map 
+    // https://developers.google.com/maps/documentation/javascript/adding-a-google-map?hl=fr#maps_add_map-javascript
+    async initMap(id: string){
+        const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+        this.googleMap = new Map(document.getElementById(id) as HTMLElement, {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            },
+            mapId: 'CustomMap'
+        });
+    }
+
+    async addMarker(mappable: Mappable): Promise<void>{
+        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+        new AdvancedMarkerElement({
+            map:this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        })
+    }
+
 }
+
--- maps/src/index.js
+++ maps/src/index.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference types="@types/google.maps" />
-/* import { User } from './User';
-import { Company } from './Company'; */
+var User_1 = require("./User");
+var Company_1 = require("./Company");
 var CustomMap_1 = require("./CustomMap");
-new CustomMap_1.CustomMap('map');
+var user = new User_1.User();
+var company = new Company_1.Company();
+var customMap = new CustomMap_1.CustomMap('map');
+customMap.addMarker(user);
+customMap.addMarker(company);
