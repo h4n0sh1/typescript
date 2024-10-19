19217dda491e69a84222de020217b79ec00c0d36
h4n0sh1 <hanchi.oth@gmail.com>
2024-10-19 22:51:14 +0200

    typos

--- sort/index.ts
+++ sort/index.ts
@@ -3,7 +3,7 @@ import { NumberCollection } from './NumberCollection';
 import { CharacterCollection } from './StringCollection';
 
 const numberCollection = new NumberCollection([10, 3, -5, 0, 30, 5])
-const stringCollection = new CharacterCollection("ZaXnj0nRsTbBC")
+const characterCollection = new CharacterCollection("ZaXnj0nRsTbBC")
 const sorter = new Sorter(stringCollection);
 sorter.sort();
 console.log(sorter.collection);
