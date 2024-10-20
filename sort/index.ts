50a0af0580d6b931bc04b7b458611d101ea05e3e
h4n0s1 <hanchi.oth@gmail.com>
2024-10-20 12:13:45 +0200

    Rearrange + Renaming files

--- sort/index.ts
+++ sort/index.ts
@@ -1,6 +1,6 @@
 import { Sorter } from './Sorter';
-import { NumberCollection } from './NumberCollection';
-import { CharacterCollection } from './StringCollection';
+import { NumberCollection } from './src/NumberCollection';
+import { CharacterCollection } from './src/CharacterCollection';
 
 const numberCollection = new NumberCollection([10, 3, -5, 0, 30, 5])
 const characterCollection = new CharacterCollection("ZaXnj0nRsTbBC")
