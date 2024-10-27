0bf07b1a540a1b9c5a4c2326bda17b14e84a1fcf
h4n0s1 <hanchi.oth@gmail.com>
2024-10-27 13:41:16 +0100

    Adds MatchResult Object for better logical coverage

--- stats/build/index.js
+++ stats/build/index.js
@@ -12,14 +12,17 @@ const matches = fs_1.default
     .map((row) => {
     return row.split(',');
 });
-const homeWin = 'H';
-const awayWin = 'A';
+const MatchResult = {
+    HomeWin: 'H',
+    AwayWin: 'A',
+    Draw: 'D'
+};
 let manUnitedWins = 0;
 for (let match of matches) {
-    if (match[1] === 'Man United' && match[5] === homeWin) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === awayWin) {
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
