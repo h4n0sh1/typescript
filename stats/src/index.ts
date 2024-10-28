7c38ad65cb97a1851b4195ccb7dd3c7b0f8a41df
h4n0s1 <hanchi.oth@gmail.com>
2024-10-28 12:12:19 +0100

    Adds MatchData tuple and MatchResult type assertion

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,10 @@
 import fs from 'fs';
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from  './MatchResult';
 
 const reader = new  CsvFileReader('./data/football.csv');
 reader.read();
 
-enum MatchResult {
-    HomeWin = 'H',
-    AwayWin = 'A',
-    Draw = 'D'
-};
 
 let manUnitedWins = 0;
 
