54af9771be99c835b9abcf2a5fd32be0e49d27f3
h4n0sh1 <hanchi.oth@gmail.com>
2024-11-02 14:58:23 +0100

    Putting it all together

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,10 +1,17 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
 import { CsvFileReader } from "./CsvFileReader";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
 
 const csvFileReader = new CsvFileReader("./data/football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
