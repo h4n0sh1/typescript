1178070a1219a76c4a4d0349e5b5316bd92e07a5
h4n0sh1 <hanchi.oth@gmail.com>
2024-11-02 17:04:22 +0100

    Implements static method for Summary

--- stats/build/index.js
+++ stats/build/index.js
@@ -2,11 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
 const CsvFileReader_1 = require("./CsvFileReader");
-const HtmlReport_1 = require("./reportTargets/HtmlReport");
-const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 const Summary_1 = require("./Summary");
 const csvFileReader = new CsvFileReader_1.CsvFileReader("./data/football.csv");
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis("Man United"), new HtmlReport_1.HtmlReport());
+const summary = Summary_1.Summary.winsAnalysisWithHtmlReport("Man United");
 summary.buildAndPrintReport(matchReader.matches);
