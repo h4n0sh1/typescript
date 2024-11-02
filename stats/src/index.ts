e4496b679ab210bc9541afe695bb9407105519c1
h4n0sh1 <hanchi.oth@gmail.com>
2024-11-02 14:48:21 +0100

    Implements 1st Analyzer : WinsAnalysis

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,10 @@
-import { MatchReader }  from  './MatchReader';
-import { MatchResult } from  './MatchResult';
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
 
-const csvFileReader = new CsvFileReader('./data/football.csv');
+const csvFileReader = new CsvFileReader("./data/football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches){
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin){
-        manUnitedWins ++;
-    }else if(match[2]  === 'Man United' && match[5] === MatchResult.AwayWin){
-        manUnitedWins ++;
-    }
-}
-
-console.log(`Man United won ${manUnitedWins} games`)
+console.log(`Man United won ${manUnitedWins} games`);
